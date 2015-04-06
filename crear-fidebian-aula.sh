ef3c4472e3f93d01f183468d88cacbbf5130c9da
root <root@mk-fi-deb-32.(none)>
2015-04-07 01:31:51 +0200

    mejoramos netbeans

--- crear-fidebian-aula.sh
+++ crear-fidebian-aula.sh
@@ -66,6 +66,9 @@ chroot ${DIR} apt-get -y install eclipse build-essential php5 wxmaxima scilab em
 
 chroot ${DIR} apt-get -y install ghc hugs manpages-dev phpmyadmin swi-prolog vim-gtk squeak-vm fpc gnat wireshark
 
+# Instalar Paquetes necesarios para cups y escaner
+chroot ${DIR} apt-get -y install cups system-config-printer hplip sane libsane-dev sane-utils xsane
+
 # Esto es un workaround : Volvemos a agregar los DNS server from google
 echo "nameserver 8.8.8.8
 nameserver 8.8.4.4" > ${DIR}/etc/resolv.conf 
@@ -75,22 +78,20 @@ nameserver 8.8.4.4" > ${DIR}/etc/resolv.conf
 # instalamos software adicional como netbeans desde unstable
 echo 'deb http://ftp.us.debian.org/debian/ unstable main contrib non-free
  ' >> ${DIR}/etc/apt/sources.list
- chroot ${DIR} apt-get update
+chroot ${DIR} apt-get update
 chroot ${DIR} apt-get -y install netbeans
-crear_listado_de_repos
-chroot ${DIR} apt-get -y purge netbeans
 
-if ! [[ -a netbeans-8.0.2.tar.gz ]] ; then 
-	echo "El archivo netbeans tar.gz NO EXISTE"
+if ! [[ -a netbeans-8.0.2-linux.sh ]] ; then 
+	echo "El archivo netbeans-8.0.2-linux.sh NO EXISTE"
 	exit 1
 fi
 
-cp netbeans-8.0.2.tar.gz ${DIR}/tmp/
-cd ${DIR}/usr/ && tar xvzf ../tmp/netbeans-8.0.2.tar.gz
-cd ../../
+cp netbeans-8.0.2-linux.sh ${DIR}/tmp/
+chroot ${DIR} sh /tmp/netbeans-8.0.2-linux.sh --silent
 
-# Instalar Paquetes necesarios para cups y escaner
-chroot ${DIR} apt-get -y install cups system-config-printer hplip sane libsane-dev sane-utils xsane
+# Desinstalamos el netbeans original de Debian
+crear_listado_de_repos
+chroot ${DIR} apt-get -y purge netbeans
 
 
 # Quitamos los archives de paquetes bajados
