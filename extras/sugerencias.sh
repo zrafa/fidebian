f07bd9cee3cfe516ae2685abeaf0bc0a6c151a47
root <root@mk-fi-deb-32.(none)>
2015-04-20 23:17:22 +0200

    solucionamos el nombre del paquete

--- extras/sugerencias.sh
+++ extras/sugerencias.sh
@@ -1,2 +1,2 @@
-sudo apt-get -y install scilab cups system-config-printer hplip sane libsane-dev sane-utils xsane gnat ghc wireshark lyx latexila dia texlive-lang-spanish imagemagick orca
+sudo apt-get -y install scilab cups system-config-printer hplip sane libsane-dev sane-utils xsane gnat ghc wireshark lyx latexila dia texlive-lang-spanish imagemagick gnome-orca
 
